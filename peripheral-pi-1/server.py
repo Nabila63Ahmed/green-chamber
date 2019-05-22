ebc96908cb453a9924eed06183a491bb0404256c
Amil Imeri <amil_imeri@hotmail.com>
2019-05-22 22:41:44 +0200

    server.py changed

--- peripheral-pi-1/server.py
+++ peripheral-pi-1/server.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python
 import pika
-import sys
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+channel.exchange_declare(exchange='topic_data', exchange_type='topic')
 
-routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
-message = ' '.join(sys.argv[2:]) or 'Hello World!'
-channel.basic_publish(
-    exchange='topic_logs', routing_key=routing_key, body=message)
-print(" [x] Sent %r:%r" % (routing_key, message))
-connection.close()
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='topic_data', queue=queue_name, routing_key='sensor.pir')
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+    channel.basic_publish(exchange='topic_data', routing_key='actuator.lcd', body='Room occupied!!!')
+    print(" [x] Sent 'actuator.lcd':'Room occupied!!!'")
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
