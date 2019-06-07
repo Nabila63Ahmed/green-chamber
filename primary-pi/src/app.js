d8882c092783a8b7c6af36b3a7bfef811616a719
Nabila Ahmed <nabila.ahmed63@gmail.com>
2019-06-07 02:19:49 +0200

    :construction: Specific exchanges, queues and routing keys

--- primary-pi/src/app.js
+++ primary-pi/src/app.js
@@ -6,35 +6,102 @@ import api from './api';
 
 (async () => {
   const connectionUri = 'amqp://localhost';
-  const exchangeName = 'exchange-1';
-  const queueName = 'queue-1';
-  const routingKey = '*';
+  const exchangeName1 = 'sensors-exchange';
+  const exchangeName2 = 'actuators-exchange';
+  const exchangeName3 = 'api-exchange';
+
+  const queueName1 = 'sensors.temperature.queue';
+  const queueName2 = 'sensors.humidity.queue';
+  const queueName3 = 'sensors.motion.queue';
+  const queueName4 = 'actuators.plugwise.lamp.queue';
+  const queueName5 = 'actuators.plugwise.fan.queue';
+  const queueName6 = 'actuators.lcd.queue';
+  const queueName7 = 'api.google.calender.queue';
+
+  const routingKey1 = 'sensors.temperature';
+  const routingKey2 = 'sensors.humidity';
+  const routingKey3 = 'sensors.motion';
+  const routingKey4 = 'actuators.plugwise.lamp';
+  const routingKey5 = 'actuators.plugwise.fan';
+  const routingKey6 = 'actuators.lcd';
+  const routingKey7 = 'api.google.calender';
 
   const connection = await amqp.createConnection({ connectionUri });
   const channel = await amqp.createChannel({ connection });
 
-  await amqp.assertExchange({ channel, exchangeName });
-  await amqp.assertQueue({ channel, queueName });
+  await amqp.assertExchange({ channel, exchangeName: exchangeName1 });
+  await amqp.assertExchange({ channel, exchangeName: exchangeName2 });
+  await amqp.assertExchange({ channel, exchangeName: exchangeName3 });
+
+  await amqp.assertQueue({ channel, queueName: queueName1 });
+  await amqp.assertQueue({ channel, queueName: queueName2 });
+  await amqp.assertQueue({ channel, queueName: queueName3 });
+  await amqp.assertQueue({ channel, queueName: queueName4 });
+  await amqp.assertQueue({ channel, queueName: queueName5 });
+  await amqp.assertQueue({ channel, queueName: queueName6 });
+  await amqp.assertQueue({ channel, queueName: queueName7 });
 
   await amqp.bindQueueToExchange({
     channel,
-    exchangeName,
-    queueName,
-    routingKey,
+    exchangeName: exchangeName1,
+    queueName: queueName1,
+    routingKey: routingKey1,
+  });
+
+  await amqp.bindQueueToExchange({
+    channel,
+    exchangeName: exchangeName1,
+    queueName: queueName2,
+    routingKey: routingKey2,
+  });
+
+  await amqp.bindQueueToExchange({
+    channel,
+    exchangeName: exchangeName1,
+    queueName: queueName3,
+    routingKey3,
+  });
+
+  await amqp.bindQueueToExchange({
+    channel,
+    exchangeName: exchangeName2,
+    queueName: queueName4,
+    routingKey: routingKey4,
+  });
+
+  await amqp.bindQueueToExchange({
+    channel,
+    exchangeName: exchangeName2,
+    queueName: queueName5,
+    routingKey: routingKey5,
+  });
+
+  await amqp.bindQueueToExchange({
+    channel,
+    exchangeName: exchangeName2,
+    queueName: queueName6,
+    routingKey: routingKey6,
+  });
+
+  await amqp.bindQueueToExchange({
+    channel,
+    exchangeName: exchangeName3,
+    queueName: queueName7,
+    routingKey: routingKey7,
   });
 
   await amqp.publish({
     channel,
-    exchangeName,
-    routingKey,
-    messageJSON: { message: 'Hello World I' },
+    exchangeName: exchangeName1,
+    routingKey: routingKey1,
+    messageJSON: { value: 25.4, createdAt: 647284 },
   });
 
   await amqp.publish({
     channel,
-    exchangeName,
-    routingKey,
-    messageJSON: { message: 'Hello World II' },
+    exchangeName: exchangeName1,
+    routingKey: routingKey2,
+    messageJSON: { value: 80, createdAt: 789800 },
   });
 
   const handleMessageReceived = message => {
@@ -49,6 +116,7 @@ import api from './api';
       const messageJSON = JSON.parse(message.content.toString());
 
       console.log('MESSAGE >', messageJSON);
+      console.log('ROUTING KEY >', message.fields.routingKey);
 
       // TODO: Implement conditional logic
     }
@@ -56,7 +124,13 @@ import api from './api';
 
   amqp.consume({
     channel,
-    queueName,
+    queueName: queueName1,
+    onMessageReceived: handleMessageReceived,
+  });
+
+  amqp.consume({
+    channel,
+    queueName: queueName2,
     onMessageReceived: handleMessageReceived,
   });
 })();
@@ -65,7 +139,7 @@ const mongodbConnectionString = 'mongodb://127.0.0.1/green-chamber';
 mongoose.connect(mongodbConnectionString, { useNewUrlParser: true });
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
