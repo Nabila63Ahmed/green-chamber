8f4dee3272417f5898a81c24c511bdf333a5cc0f
Nabila Ahmed <nabila.ahmed63@gmail.com>
2019-06-26 01:59:23 +0200

    :construction: Successful network request

--- admin-system/src/App.js
+++ admin-system/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return <h1>Hello World</h1>;
+class App extends React.Component {
+  state = {};
+
+  async componentDidMount() {
+    const response = await axios.get('http://localhost:4000/api/ping');
+    console.log(response);
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
 
 export default App;
