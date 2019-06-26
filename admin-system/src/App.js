0f204cb5aa2c709fb8844d4135757f7e563ee98f
Nabila Ahmed <nabila.ahmed63@gmail.com>
2019-06-26 19:55:31 +0200

    :lipstick: Temperature and Humidity graphs in UI

--- admin-system/src/App.js
+++ admin-system/src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { LineChart, XAxis, YAxis, CartesianGrid, Line, Label } from 'recharts';
 import moment from 'moment';
+import { getTemperatures, getHumidities } from './networking';
 
 class App extends React.Component {
-  state = {};
+  state = {
+    temperatures: [],
+    humidities: [],
+  };
+
+  async componentDidMount() {
+    const temperatureRecords = await getTemperatures();
+    const modifiedTemperatureRecords = temperatureRecords.map(record => ({
+      ...record,
+      createdAt: moment(record.createdAt).format('LT'),
+    }));
+    this.setState({ temperatures: modifiedTemperatureRecords });
+
+    const humidityRecords = await getHumidities();
+    const modifiedHumityRecords = humidityRecords.map(record => ({
+      ...record,
+      createdAt: moment(record.createdAt).format('LT'),
+    }));
+    this.setState({ humidities: modifiedHumityRecords });
+  }
 
   render() {
     return (
       <div>
         <LineChart
-          width={1000}
+          width={400}
           height={300}
           margin={{ top: 30, bottom: 30, left: 30, right: 30 }}
-          data={temperatures}
+          data={this.state.temperatures}
         >
           <XAxis dataKey="createdAt">
             <Label value="Time" offset={-10} position="insideBottom" />
@@ -24,14 +44,25 @@ class App extends React.Component {
 
           <Line type="monotone" dataKey="value" stroke="#8884d8" />
         </LineChart>
+        <LineChart
+          width={400}
+          height={300}
+          margin={{ top: 30, bottom: 30, left: 30, right: 30 }}
+          data={this.state.humidities}
+        >
+          <XAxis dataKey="createdAt">
+            <Label value="Time" offset={-10} position="insideBottom" />
+          </XAxis>
+
+          <YAxis>
+            <Label value="Humidity" angle={-90} position="insideLeft" />
+          </YAxis>
+
+          <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        </LineChart>
       </div>
     );
   }
 }
-
-const temperatures = new Array(30).fill(0).map(() => ({
-  value: Math.random() * 25 + 15,
-  createdAt: moment().format('LT'),
-}));
 
 export default App;
--- admin-system/src/networking/index.js
+++ admin-system/src/networking/index.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
-export const getTemperatures = () => {
-  return axios.get('http://localhost:4000/api/temperature');
-}
+export const getTemperatures = async () => {
+  const response = await axios.get('http://localhost:4000/api/temperature');
+  return response.data.data.temperatureRecords;
+};
+
+export const getHumidities = async () => {
+  const response = await axios.get('http://localhost:4000/api/humidity');
+  return response.data.data.humidityRecords;
+};
