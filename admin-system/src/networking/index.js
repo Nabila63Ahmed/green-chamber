0970fbe61002a2e315e3a5a64e3b2c6e1ebecb68
Nabila Ahmed <nabila.ahmed63@gmail.com>
2019-06-30 01:45:26 +0200

    :hammer: Fetch today's readings

--- admin-system/src/networking/index.js
+++ admin-system/src/networking/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
 export const getTemperatures = async () => {
-  const response = await axios.get('http://localhost:4000/api/temperature');
+  const response = await axios.get(
+    'http://localhost:4000/api/temperature/today',
+  );
+
   return response.data.data.temperatureRecords;
 };
 
 export const getHumidities = async () => {
-  const response = await axios.get('http://localhost:4000/api/humidity');
+  const response = await axios.get('http://localhost:4000/api/humidity/today');
   return response.data.data.humidityRecords;
 };
