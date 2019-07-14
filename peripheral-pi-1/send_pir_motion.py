a262ec3149b8d670678048e620a292ef3351fd6d
Amil Imeri <amil_imeri@hotmail.com>
2019-07-14 15:30:55 +0200

    Updated motion script - set time interval

--- peripheral-pi-1/send_pir_motion.py
+++ peripheral-pi-1/send_pir_motion.py
@@ -13,31 +13,52 @@ exchange_name = 'sensors-exchange'
 routing_key_name = 'sensors.motion'
 
 pir_sensor = 8
-motion=0
+timer = 30
+last_motion_message = 0
+motion_in_between = False
+motion = 0
 grovepi.pinMode(pir_sensor,"INPUT")
 
 while True:
     try:
         motion=grovepi.digitalRead(pir_sensor)
         if motion==0 or motion==1:
-            now = int(round(time.time() * 1000)) # Current time in miliseconds
             if motion==1:
-                message = {
-                        "value": 1,
-                        "createdAt": now
-                        }
-                message_json = json.dumps(message)
                 print ('Motion Detected')
+                if last_motion_message==0:
+                    now = int(round(time.time() * 1000)) # Current time in miliseconds
+                    message = {
+                            "value": 1,
+                            "createdAt": now
+                            }
+                    message_json = json.dumps(message)
+                    channel.basic_publish(exchange=exchange_name, routing_key=routing_key_name, body=message_json)
+                    print(" [x] Sent %r:%r" % (routing_key_name, message_json))
+                    last_motion_message = 1
+                    timer = 30
+                    motion_in_between = False
+                else:
+                    motion_in_between = True
             else:
-                message = {
-                        "value": 0,
-                        "createdAt": now
-                        }
-                message_json = json.dumps(message)
                 print ('-')
-            channel.basic_publish(exchange=exchange_name, routing_key=routing_key_name, body=message_json)
-            print(" [x] Sent %r:%r" % (routing_key_name, message_json))
-        time.sleep(5)
+                if last_motion_message==1:
+                    if timer==0:
+                        if not motion_in_between:
+                            now = int(round(time.time() * 1000)) # Current time in miliseconds
+                            message = {
+                                    "value": 0,
+                                    "createdAt": now
+                                    }
+                            message_json = json.dumps(message)
+                            channel.basic_publish(exchange=exchange_name, routing_key=routing_key_name, body=message_json)
+                            print(" [x] Sent %r:%r" % (routing_key_name, message_json))
+                            last_motion_message = 0
+                        else:
+                            timer = 30
+                            motion_in_between = False
+            if last_motion_message==1 and timer > 0:
+                timer = timer - 1
+        time.sleep(1)
         print(motion)
     except IOError:
         print ("Error")
